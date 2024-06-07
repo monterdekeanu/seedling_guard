9429866689fcc01e0044f14df22c281ca8d9829d
monterdekeanu <monterdekeanu@gmail.com>
2024-06-07 19:45:12 +0800

    Add DHT11 sensor readings to Tkinter GUI

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ DHT_PIN = 16
 
 
 def read_dht11():
-    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
     if humidity is not None and temperature is not None:
         print(f"Temperature: {temperature:.1f}C, Humidity: {humidity:.1f}%")
@@ -19,6 +19,6 @@ if __name__ == "__main__":
     try:
         while True:
             read_dht11()
-            time.sleep(2)  # Read data every 2 seconds
+            time.sleep(1)  # Read data every 1 seconds
     except KeyboardInterrupt:
         print("Program terminated")
